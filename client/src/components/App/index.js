4237251ed1a6232721df60a8ea74fe0fe09a4c5f
etaranen <118066128+etaranen@users.noreply.github.com>
2024-07-19 19:39:25 +0000

    directories, appbar, and navigation added

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import Review from './Review';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from '../Landing';
+import Search from '../Search';
+import Review from '../Review';
+import MyPage from '../MyPage';
 
-function App() {
-  return <Review />;
-}
+
+const App = () => {
+    return (
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/Search" element={<Search />} />
+            <Route path="/Review" element={<Review />} />
+            <Route path="/MyPage" element={<MyPage />} />
+          </Routes>
+        </div>
+      </Router>
+    );
+  };
 
 export default App;
--- server.js
+++ server.js
@@ -6,21 +6,31 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import bodyParser from 'body-parser';
 import response from 'express';
+import cors from 'cors';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
+app.use(cors());
 app.use(express.static(path.join(__dirname, "client/build")));
 
+const connection = mysql.createConnection(config);
+
+connection.connect((err) => {
+	if (err) {
+	  console.error('Error connecting to the database:', err);
+	  return;
+	}
+	console.log('Connected to the database');
+  });
 
 app.post('/api/loadUserSettings', (req, res) => {
 
-	let connection = mysql.createConnection(config);
 	let userID = req.body.userID;
 
 	let sql = `SELECT mode FROM user WHERE userID = ?`;
@@ -37,8 +47,43 @@ app.post('/api/loadUserSettings', (req, res) => {
 		//let obj = JSON.parse(string);
 		res.send({ express: string });
 	});
-	connection.end();
 });
+
+
+// Endpoint to get movies
+app.post('/api/getMovies', (req, res) => {
+
+	let sql = 'SELECT * FROM movies';
+
+	connection.query(sql, (error, results) => {
+	  if (error) {
+		return console.error('Error fetching movies:', error);
+	  }
+	  res.send(results);
+	});
+  });
+
+// Endpoint to add reviews
+app.post('/api/addReview', (req, res) => {
+
+	const { movieID, userID, reviewTitle, reviewContent, reviewScore } = req.body;
+	
+	let sql = `
+		INSERT INTO Review (reviewTitle, reviewContent, reviewScore, userID, movieID)
+		VALUES (?, ?, ?, ?, ?)
+		`;
+	
+	let values = [reviewTitle, reviewContent, reviewScore, userID, movieID];
+
+	connection.query(sql, values, (error, results) => {
+		if (error) {
+			return console.error('Error adding review:', error);
+		}
+		res.json('Success!');
+	});
+	// connection.end();
+});
+
 
 
 
